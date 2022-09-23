3baac55022de109429a446dc42cca1e4fb028429
Dusan <dusanvulic11@gmail.com>
2022-09-23 13:42:44 +0200

    adding delete button to edit expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect } from "react";
 
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+//icons
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -12,7 +14,37 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
-  return <Text> manage expense</Text>;
+  const deleteExpenseHandler = () => {};
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <FontAwesome5
+            name="trash"
+            size={30}
+            color="darkred"
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    // backgroundColor: "white",
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 16,
+
+    borderTopColor: "gray",
+    borderTopWidth: 1,
+    alignItems: "center",
+  },
+});
 export default ManageExpense;
