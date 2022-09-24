885a53588c76f255700418548d793623290e23cb
Dusan <dusanvulic11@gmail.com>
2022-09-24 17:53:33 +0200

    added some styling to input fields

--- components/Input.js
+++ components/Input.js
@@ -1,13 +1,42 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 const Input = ({ label, textInputConfig }) => {
+  let inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput {...textInputConfig} style={inputStyles} />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 12,
+    color: "green",
+    marginBottom: 6,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: "green",
+    fontSize: 18,
+  },
+
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
 export default Input;
